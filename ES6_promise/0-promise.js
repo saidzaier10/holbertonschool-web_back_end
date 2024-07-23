0bf4d41381425e86277b0317eb6faa98552da3bb
SaidZaier <8770@holbertonstudents.com>
2024-07-23 18:18:18 +0200

    fix eslint task 1

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Simulate an API call with a timeout
     setTimeout(() => {
       resolve('Response from API');
