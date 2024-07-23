c605a80adb834adcbe51938a00b95d5873a7b273
SaidZaier <8770@holbertonstudents.com>
2024-07-23 18:43:03 +0200

    load balancer fix task 7

--- ES6_promise/7-load-balancer.js
+++ ES6_promise/7-load-balancer.js
@@ -1,3 +1,10 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]);
+  return new Promise((resolve) => {
+    const china = chinaDownload();
+    const US = USDownload();
+
+    Promise.race([china, US]).then((result) => {
+      resolve(result);
+    });
+  });
 }
