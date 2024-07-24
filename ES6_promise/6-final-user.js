278a6e31e0483811e1748e32a9516de1ecf0cbc0
SaidZaier <8770@holbertonstudents.com>
2024-07-24 13:56:23 +0200

    fix eslint into the task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,10 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName).then((user) => ({ status: 'fulfilled', value: user }));
+  const userPromise = signUpUser(firstName, lastName).then(user => ({ status: 'fulfilled', value: user }));
   const photoPromise = uploadPhoto(fileName).then(
     () => ({ status: 'fulfilled', value: null }),
-    (err) => ({ status: 'rejected', value: `${err.name}: ${err.message}` }),
+    err => ({ status: 'rejected', value: `${err.name}: ${err.message}` })
   );
 
   return Promise.all([userPromise, photoPromise]);
