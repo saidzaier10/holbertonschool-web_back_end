4150434970e06ee05437679d5eba358da0b4a2d3
SaidZaier <8770@holbertonstudents.com>
2024-12-16 10:21:29 +0100

    a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module containing the add function.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the result as a float.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
