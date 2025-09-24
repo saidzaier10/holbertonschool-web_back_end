be6a6cb457f0eee00bc2e19c840b714009fba796
SaidZaier <8770@holbertonstudents.com>
2025-09-24 08:42:01 +0200

    fix eslint

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,33 +1,31 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (error, data) => {
-      if (error) {
-        reject(error);
-        return;
-      }
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+      reject(error);
+      return;
+    }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
-      const fields = {};
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+    const fields = {};
 
-      students.forEach((student) => {
-        const studentData = student.split(',');
-        if (studentData.length === 4 && studentData[0]) {
-          const firstname = studentData[0];
-          const field = studentData[3];
+    students.forEach((student) => {
+      const studentData = student.split(',');
+      if (studentData.length === 4 && studentData[0]) {
+        const firstname = studentData[0];
+        const field = studentData[3];
 
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstname);
+        if (!fields[field]) {
+          fields[field] = [];
         }
-      });
-
-      resolve(fields);
+        fields[field].push(firstname);
+      }
     });
+
+    resolve(fields);
   });
-};
+});
 
 export default readDatabase;
