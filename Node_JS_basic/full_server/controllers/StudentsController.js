b0a8bd100987850373902a4b08829351cb745161
SaidZaier <8770@holbertonstudents.com>
2025-09-24 08:46:21 +0200

    fix elint StudentController

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -9,7 +9,8 @@ class StudentsController {
         let output = 'This is the list of our students\n';
 
         // Sort fields alphabetically (case insensitive)
-        const sortedFields = Object.keys(fields).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        const sortedFields = Object.keys(fields).sort((a, b) => a
+          .toLowerCase().localeCompare(b.toLowerCase()));
 
         sortedFields.forEach((field) => {
           const list = fields[field];
